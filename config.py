43613b8fb5485bb432d50eee5d413658dc4741dd
speedyindex_clone <hadirealstate590@gmail.com>
2025-09-06 02:05:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 BING_API_KEY = 'C507D6C08ABF84479FEF3FB48B45044A'
 SITE_URL = 'https://github.com/hadirealstate5/Indexing.git'
-
 GITHUB_REPO = 'https://github.com/hadirealstate5/Indexing.git'
+
